ba493370e7ef5bd6494035b41fe1e49df5b3d41b
yukai06 <yukai06@58.com>
2024-08-01 16:06:50 +0800

    del: 删除无用代码

---
+++ src/components/calendar-mini/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+function Calendar() {
+  return (
+    <div className="calendar">
+      <h2>Calendar</h2>
+    </div>
+  );
+}
+
+export default Calendar;
