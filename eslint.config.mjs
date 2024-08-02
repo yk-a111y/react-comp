82cfdcf437ef14e1e6b5207fa3445f0f513df05c
yukai06 <yukai06@58.com>
2024-08-02 10:33:37 +0800

    chore: 配置调整

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,4 @@
-import globals from "globals";
+import globals, { browser, es2021 } from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
@@ -8,6 +8,8 @@ export default [
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   {
     languageOptions: { globals: globals.browser }, env: {
+      browser: true,
+      es2021: true,
       node: true
     }
   },
