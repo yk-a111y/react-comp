a5b7e5bab237587c6c8a9eda6f7435a3452ceff3
yukai06 <yukai06@58.com>
2024-08-06 14:28:27 +0800

    chore: 调整项目结构

---
+++ src/CalendarApp.tsx
@@ -0,0 +1,40 @@
+import React, { useRef, useEffect, useState } from "react";
+import { CalendarRef } from "@/components/calendar-mini/index";
+// import Calendar from "@/components/calendar-mini/index";
+import Calendar from "@/components/calendar/index";
+import dayjs from "dayjs";
+
+// const CalendarMini = React.forwardRef(Calendar);
+
+function CalendarApp() {
+  const [date, setDate] = useState(new Date());
+  const calendarRef = useRef<CalendarRef>(null);
+
+  // useEffect(() => {
+  // console.log(calendarRef.current?.getDate().toLocaleDateString());
+
+  // 外部调用组件内部方法
+  // setTimeout(() => {
+  //   calendarRef.current?.setDate(new Date(2024, 3, 1));
+  // }, 3000);
+  // }, []);
+
+  // const handleDateChange = (value: Date) => {
+  //   console.log("🚀 ~ handleDateChange ~ value:", value);
+  //   setDate(value);
+  // };
+
+  return (
+    <div className="App">
+      {/* <CalendarMini ref={calendarRef} defaultValue={new Date()} />
+      <CalendarMini
+        ref={calendarRef}
+        value={date}
+        onChange={handleDateChange}
+      /> */}
+      <Calendar value={dayjs("2024-8-5")} />
+    </div>
+  );
+}
+
+export default CalendarApp;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import CalendarApp from "./CalendarApp";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+root.render(<CalendarApp />);
